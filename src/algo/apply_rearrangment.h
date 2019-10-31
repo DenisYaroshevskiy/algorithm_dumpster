671f973f817c62c81e1e554f254d9b7929990ffa
Denis Yaroshevskiy <denis.yaroshevskij@gmail.com>
2019-10-31 21:56:26 +0000

    fixing concepts for apply_rearrangment

--- src/algo/apply_rearrangment.h
+++ src/algo/apply_rearrangment.h
@@ -27,17 +27,16 @@
 namespace algo {
 namespace detail {
 
-template <typename II, typename Op>
-// require RandomAccessPositionIteator<II> &&
-//         IteratorToPosition<Op, ValueType<I>>
-constexpr void cycle_from_position(II f, II cur, Op original_pos,
+template <typename II>
+// require RandomAccessIteator<II> && Position<ValueType<II>
+constexpr void cycle_from_position(II f, II cur, ValueType<II> base,
                                    ValueType<II> marker) {
   using N = DifferenceType<II>;
   using I = ValueType<II>;
   using T = ValueType<I>;
 
   const N start = cur - f;
-  N next_n = N{original_pos(*cur)};
+  N next_n = N{*cur - base};
 
   if (next_n == start) return;
 
@@ -48,23 +47,22 @@ constexpr void cycle_from_position(II f, II cur, Op original_pos,
     **cur = std::move(**next_cur);
     *cur = marker;
     cur = next_cur;
-    next_n = N{original_pos(*cur)};
+    next_n = N{*cur - base};
   } while (next_n != start);
 
   **cur = std::move(tmp);
   *cur = marker;
 }
 
-template <typename II, typename Op>
-// require RandomAccessPositionIteator<II> &&
-//         IteratorToPosition<Op, ValueType<I>>
-constexpr void cycle_from_position_no_marker(II f, II cur, Op original_pos) {
+template <typename II>
+// require RandomAccessIteator<II> && Position<ValueType<II>
+constexpr void cycle_from_position_no_marker(II f, II cur, ValueType<II> base) {
   using N = DifferenceType<II>;
   using I = ValueType<II>;
   using T = ValueType<I>;
 
   const N start = cur - f;
-  N next_n = N{original_pos(*cur)};
+  N next_n = N{*cur - base};
 
   if (next_n == start) return;
 
@@ -76,7 +74,7 @@ constexpr void cycle_from_position_no_marker(II f, II cur, Op original_pos) {
     **cur = std::move(**next_cur);
     std::swap(*cur, marker);
     cur = next_cur;
-    next_n = N{original_pos(*cur)};
+    next_n = N{*cur - base};
   } while (next_n != start);
 
   **cur = std::move(tmp);
@@ -107,46 +105,28 @@ constexpr void apply_rearrangment_move(II f, II l, O o) {
   }
 }
 
-template <typename II, typename Op>
-// require RandomAccessPositionIteator<II> &&
-//         IteratorToPosition<Op, ValueType<I>>
-auto apply_rearrangment(II f, II l, Op original_pos, ValueType<II> marker)
-    -> std::enable_if_t<std::is_invocable_v<Op, ValueType<II>>> {
+template <typename II>
+// require RandomAccessIteator<II> && Position<ValueType<II>
+auto apply_rearrangment(II f, II l, ValueType<II> base, ValueType<II> marker) {
   // precondition: iterators from *f to *l are a permutation
   //               of an increasing iterator sequence
   //               and pos returns number of an element in that
   //               orignal sequence.
   II cur = f;
   while (cur != l) {
-    detail::cycle_from_position(f, cur, original_pos, marker);
+    detail::cycle_from_position(f, cur, base, marker);
     cur = std::find_if(++cur, l, [&](Reference<II> x) { return x != marker; });
   }
 }
 
 template <typename II>
-// require RandomAccessPositionIteator<II> &&
-//         ForwardNIterator<ValueType<II>
-void apply_rearrangment(II f, II l, ValueType<II> base, ValueType<II> marker) {
-  apply_rearrangment(f, l, [&](ValueType<II> x) { return x - base; }, marker);
-}
-
-template <typename II, typename Op>
-// require RandomAccessPositionIteator<II> &&
-//         IteratorToPosition<Op, ValueType<I>>
-constexpr auto apply_rearrangment_no_marker(II f, II l, Op original_pos)
-    -> std::enable_if_t<std::is_invocable_v<Op, ValueType<II>>> {
+// require RandomAccessIteator<II> && Position<ValueType<II>
+constexpr auto apply_rearrangment_no_marker(II f, II l, ValueType<II> base) {
   II cur = f;
   while (cur != l) {
-    detail::cycle_from_position_no_marker(f, cur, original_pos);
+    detail::cycle_from_position_no_marker(f, cur, base);
     ++cur;
   }
-}
-
-template <typename II>
-// require RandomAccessPositionIteator<II> &&
-//         ForwardNIterator<ValueType<II>
-constexpr void apply_rearrangment_no_marker(II f, II l, ValueType<II> base) {
-  apply_rearrangment_no_marker(f, l, [&](ValueType<II> x) { return x - base; });
 }
 
 }  // namespace algo
